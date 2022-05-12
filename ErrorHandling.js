bf7bc48e48899d2154e29272de6337fff510e58b
lagadapatisindhu08 <lagadapatisindhu08@gmail.com>
2022-05-12 13:01:18 +0530

    Exception Handling Examples

---
+++ ErrorHandling.js
@@ -0,0 +1,40 @@
+// function display(){
+// try {
+//   console.log("function Started");
+//   someThingText
+//   console.log("function Ended");
+// }catch(error){
+//   console.error("function error", error);
+// }
+// }
+// display();
+
+// function display(){
+//     try {
+//     setTimeout(function(){
+//       console.log("setTimeout Example");
+//       noSuchVariable
+//     },1000);
+//     }catch(error){
+//       console.error("function throw error", error);
+//     }
+//     }
+//     display();
+
+    //let val = leftExp || rightExp
+    // function getAuthentication () {
+    //     return true;
+    // }
+
+    // let userName = getAuthentication() || "Guest User";
+    //falsy values are null, undefind, 0, "", NaN, false
+    
+    //nullish operator
+    //if leftExp is null or undefind then rightExp
+    //let val = leftExp ?? rightExp;
+
+    //leftExp && rightExp (conditional rendering)
+    // if(isLoggedIn){
+    //     gotoPayment();
+    // }
+    // isLoggedIn && gotoPayment();
